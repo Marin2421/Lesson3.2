cb2817ac280182dc41d4b7edabb3957462cc7b61
ASUS <ASUS@DESKTOP-QOB7I2F>
2024-03-28 20:16:25 +0300

    Добавьте файлы проекта.

---
+++ Lesson3.2/Program.cs
@@ -0,0 +1,14 @@
+﻿//1
+int eags = 13;
+int lastEags = 9;
+int lostEags = eags - lastEags;
+Console.WriteLine(lostEags);
+//2
+int journal = 25;
+int magazine = journal + 16;
+Console.WriteLine(magazine);
+//3
+int potato = 44;
+int beet = potato / 4;
+double onion = (double)beet / 2;
+Console.WriteLine(onion);
